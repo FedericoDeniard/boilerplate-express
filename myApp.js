8fb0968f36774f98919a33e2f7181275d081ac20
FedericoDeniard <fededeniard@gmail.com>
2024-08-29 11:40:01 -0300

    send file

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ console.log("Hello World");
 
 module.exports = app;
 
+absolutePath = __dirname + "/views/index.html";
+
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
